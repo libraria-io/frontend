c0d7d7c1fc230911f1cae1c82d9d367ec459edd1
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2020-11-04 19:49:05 +0800

    Added base interface models according to backend data structure.

--- src/contracts/index.ts
+++ src/contracts/index.ts
@@ -12,4 +12,43 @@ export interface User extends Model {
 	password?: string;
 	remember_token?: string;
 	email_verified_at: Date | string;
+	authors?: Array<Author>;
+}
+
+export interface File extends Model {
+	type: string;
+	name: string;
+	size: number;
+	public: boolean;
+}
+
+export interface Category extends Model {
+	name: string;
+	books?: Array<Book>;
+}
+
+export interface Author extends Model {
+	address: string;
+	website: string;
+	email: string;
+	user_id: number;
+	user?: User;
+	books?: Array<Book>;
+}
+
+export interface Book extends Model {
+	title: string;
+	description: string;
+	tag_ids: Array<number>;
+	photo_id: number;
+	photo?: File;
+	category_id: number;
+	category?: Category;
+	author_id: number;
+	author?: Author;
+}
+
+export interface Tag extends Model {
+	name: string;
+	books?: Array<Book>;
 }
